8c86bab2f35209c0482059a03d629c9bb8aebd36
iacopoleardini <iacopo.lea@gmail.com>
2017-07-15 18:31:45 +0200

    vue

--- app/Resources/assets/js/main.js
+++ app/Resources/assets/js/main.js
@@ -1,13 +1,22 @@
 
-import React from 'react';
-import {render} from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+//import React from 'react';
+//import {render} from 'react-dom';
+//import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import App from './App'; // Our app
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+//injectTapEventPlugin();
 
 // Render the main app react component into the app div.
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-render(<App />, document.getElementById('dc-app'));
+//render(<App />, document.getElementById('dc-app'));
+
+window.dataStore = {
+  app: {
+      name: 'Dream Catchers'
+  }
+};
+
+window.dcApp = App;
